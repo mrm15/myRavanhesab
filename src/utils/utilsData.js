f8b2c015d72bd8219bc0a81bd4792ed22e4d18a1
mrm15 <mrm.learn@gmail.com>
2023-05-20 17:11:10 +0330

    Create utilsData.js

---
+++ src/utils/utilsData.js
@@ -0,0 +1,29 @@
+import f from "./UtilsFunction";
+
+
+export default f;
+
+export const addApiUrlToLocalStorage = () => {
+  if (window.location.hostname !== "localhost") {
+    localStorage.setItem(
+      "apiUrl",
+      "https://my.ravanhesab.com/myRavanhesab/"
+    );
+  } else {
+    localStorage.setItem("apiUrl",
+      // "http://localhost/Git/myRavanhesab"
+      "https://my.ravanhesab.com/myRavanhesab/"
+
+    );
+  }
+};
+
+export const prefixUrl = () => {
+  if (
+    localStorage.getItem("apiUrl") === null ||
+    localStorage.getItem("apiUrl") === undefined
+  ) {
+    addApiUrlToLocalStorage();
+  }
+  return localStorage.getItem("apiUrl");
+};
