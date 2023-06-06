c0207a2f842ade106b9f27e29f3c5447c6de7113
mrm15 <mrm.learn@gmail.com>
2023-06-06 12:43:18 +0330

    fix bug

--- src/Components/Auth/auth.js
+++ src/Components/Auth/auth.js
@@ -4,8 +4,8 @@ const AuthContext = createContext(null)
 
 
 export const AuthProvider = ({children}) => {
-  const [user, setUser] = useState((localStorage.getItem('token') !== undefined || localStorage.getItem('token') !== null))
-
+  // const [user, setUser] = useState((localStorage.getItem('token') !== undefined || localStorage.getItem('token') !== null))
+  const [user, setUser] = useState(null )
   const login = user => {
     setUser(user)
   }
